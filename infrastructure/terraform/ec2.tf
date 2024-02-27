4107f3c8bf8937ec10797a94bbb9f25fde782873
Kuba <jp@pgms.pl>
2024-02-27 07:39:26 +0000

    ansible works 1

--- infrastructure/terraform/ec2.tf
+++ infrastructure/terraform/ec2.tf
@@ -32,7 +32,8 @@ resource "aws_instance" "jp" {
       chmod u+w /var/cache/apt/archives/lock
       sudo chown ubuntu /var/lib/apt/lists/lock
       chmod u+w /var/lib/apt/lists/lock
-      sudo apt-get update && sudo apt-get install tzdata python3-apt && sudo rm /var/lib/apt/lists/lock && sudo rm /var/lib/apt/lists/lock-frontend
+      sudo apt-get update && sudo apt-get install tzdata python3-apt python3-pip python3-dev python3-apt
+      sudo rm /var/lib/apt/lists/lock && sudo rm /var/lib/apt/lists/lock-frontend
     EOF
 }
 
